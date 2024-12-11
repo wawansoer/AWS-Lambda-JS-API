6a7ea41c166533799193f058f363959080a9f774
sutisna <sutisna@softwareseni.com>
2024-12-11 14:09:09 +0700

    init

---
+++ country.js
@@ -0,0 +1,12 @@
+module.exports.handler = async () => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless v3.0! Your function executed successfully!",
+            },
+            null,
+            2
+        ),
+    };
+};
